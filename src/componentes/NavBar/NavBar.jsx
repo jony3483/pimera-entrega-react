4d2c1ac9544f15b5b314a2e3d080eeca6b5ed0cb
jony <jony.rpm@gmail.com>
2023-10-06 00:28:03 -0300

    primera entrega react css

--- src/componentes/NavBar/NavBar.jsx
+++ src/componentes/NavBar/NavBar.jsx
@@ -11,14 +11,14 @@ import styles from './styles.module.css';
 
 const NavBar = () =>   {
   return (
-    <Navbar expand="xxl" className={styles.Navbar}>
+    <Navbar expand="lg" className={styles.Navbar}>
       <Container className={styles.container}>
         <Navbar.Brand className={styles.menuNav} href="#home">TIENDA</Navbar.Brand>
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Collapse>
                 <Nav className={styles.nav}>
                     <Nav.Link className={styles.menuNav} href="#home">Home</Nav.Link>
                     <Nav.Link className={styles.menuNav} href="#link">Contacto</Nav.Link>
-                        <NavDropdown title="PRODUCTOS" id="basic-nav-dropdown">
+                        <NavDropdown title="PRODUCTOS">
                             <NavDropdown.Item className={styles.menuNav} href="#action/3.1">Jordan</NavDropdown.Item>
                             <NavDropdown.Item className={styles.menuNav} href="#action/3.2">Gorras</NavDropdown.Item>
                             <NavDropdown.Item className={styles.menuNav} href="#action/3.3">Poleras</NavDropdown.Item>
